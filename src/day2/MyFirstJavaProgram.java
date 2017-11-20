c47967c664dedba75c4ff852b9a9402372a94022
Grace <league@WTS20.attlocal.net>
2017-11-20 13:54:15 -0800

    day
    *thumbs up*

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot r2d2 = new Robot ();
 	
-		
-		
+	r2d2.penDown();
+	r2d2.sparkle();
+r2d2.setRandomPenColor();
+r2d2.move(100);
+r2d2.turn(90);
+r2d2.move(100);
+r2d2.turn(90);
+r2d2.move(100);
+r2d2.turn(90);
+r2d2.move(100);
+
+
+r2d2.miniaturize();
+
 	}
 }
--- src/day2/SmartShapes.java
+++ src/day2/SmartShapes.java
@@ -6,24 +6,30 @@ public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
+Robot terminator = new Robot();
 
         // 3. Put the robot's pen down
-
+terminator.penDown();
 
         // 6. Make the robot move as fast as possible
-
+terminator.setSpeed(100000000);
 
         // 5. Do everything below here 4 times
-
+for(int i=0; i<4; i++){
 
         //         2. Move your robot 200 pixels
-
+terminator.move(200);
 
         //         4. Turn the robot 90 degrees to the right
 
-    	
+    	terminator.turn(90);
+}
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
    
-    }
-}
+   for(int i=0; i<3; i++){
+
+
+terminator.move(200);
+    terminator.turn(120);
+   }
+    }}
